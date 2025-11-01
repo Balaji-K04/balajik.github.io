b2a6e7b3577993da2d6f67a2e07eb468f9b939f6
Balaji Krishnakumar <balajisathesh.bs@gmail.com>
2025-11-01 19:54:35 +0530

    modified the build

--- dist/balajik.github.io/server/angular-app-manifest.mjs
+++ dist/balajik.github.io/server/angular-app-manifest.mjs
@@ -2,19 +2,19 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: '/balajik.github.io/',
+  baseHref: '/',
   locale: undefined,
   routes: [
   {
     "renderMode": 2,
-    "route": "/balajik.github.io"
+    "route": "/"
   }
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 458, hash: 'acfde31beccee68ba511514139205b79019bcacd02bc3441f2b1164f4483067e', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 971, hash: '74b35b5ee9a594e74afd04027b66284f511f68fa37578a15f7c4e3c5bbb37b9c', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 21598, hash: '907952a18c6277e7194092efa443c9571f590f24d3b149b122dc6d52425ed621', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 440, hash: '81411755e56291149fcef70b5d0cc31fa0ac01f487da92f3e9d5f8e6553814c1', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 953, hash: '64a8ad28cf3dc3555cc7ceb16bc190bbedaf566a6900451c6fea0b2c9272ca40', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 21580, hash: '11de8316538067b7f50c9782f2a0acf9c89fd583be901661fbf35305669858ae', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-5INURTSO.css': {size: 0, hash: 'menYUTfbRu8', text: () => import('./assets-chunks/styles-5INURTSO_css.mjs').then(m => m.default)}
   },
 };
